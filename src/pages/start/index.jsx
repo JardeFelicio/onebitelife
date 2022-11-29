0b90d600ae908764dd82260684fedcf68914d179
JardeFelicio <86574004+JardeFelicio@users.noreply.github.com>
2022-11-28 21:21:15 -0300

    Aula 1

--- src/pages/start/index.jsx
+++ src/pages/start/index.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { View,Text,Image,ScrollView } from "react-native";
+import { View,Text,Image,ScrollView,StyleSheet } from "react-native";
 
 
 export default function Start(){
 
     return(
-        <View>
+        <View style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
+                <View style={{alignItems:"center"}}>
                     <Image
                         source={require("../../assets/icons/logo33[.png")}
+                        style= {styles.logo}
                         />
-                    <Text>
+                    <Text style={styles.descrption}>
                         Vamos transformar sua vida {"/n"} em jogo , buscando sempre   {"/n"}o melhor nivel.
                     </Text>
 
@@ -20,3 +21,24 @@ export default function Start(){
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundcolor:"rgba(21,21,21,0.98)"
+    },
+    logo:{
+        width:300,
+        heigth:60,
+        marginTop:60,
+        marginBotton:20,
+
+    },
+    descrption:{
+        color: "#FFFFFF",
+        fontSize:20,
+        textAlign:"center",
+        marginVertical:60,
+    }
+
+})
